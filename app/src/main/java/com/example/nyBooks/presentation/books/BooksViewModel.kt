9d336444ccf1e6c3240ece3a0e9ac9dc6505c4a8
brunocn <brunnonicolai@gmail.com>
2023-08-29 20:27:24 -0300

    Build Template for ViewModel initialized + MutableLiveData

--- app/src/main/java/com/example/nyBooks/presentation/books/BooksViewModel.kt
+++ app/src/main/java/com/example/nyBooks/presentation/books/BooksViewModel.kt
@@ -1,6 +1,25 @@
 package com.example.nyBooks.presentation.books
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.nyBooks.data.model.Book
 
 class BooksViewModel : ViewModel() {
+
+    val booksLiveData: MutableLiveData<List<Book>> = MutableLiveData()
+
+    fun getBooks() {
+        booksLiveData.value = createFakeBooks()
+    }
+
+    private fun createFakeBooks(): List<Book> {
+        return listOf(
+            Book("Proud and Prejudice", "Jane Austin"),
+            Book("Wuthering Heights", "Emilly Bronte"),
+            Book("The Fault in Our Stars", "John Green"),
+            Book("Five Feet Apart", "Tobias Laconis and Rachel Lippincott"),
+            Book("If I Stay", "Gayle Forman")
+        )
+    }
+
 }
